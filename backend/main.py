791235f9d34b9c7ec3cf10220edf56dab2039a41
Krishna Saxena <rio17105@gmail.com>
2025-12-12 03:49:28 +0530

    name , logo and cors urls updated

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://helixlock.onrender.com","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
